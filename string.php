b4c770d9b63dce453e7292c73cb25351545d42f1
zonfauzan <syahrulgunawan059@gmail.com>
2022-10-07 12:42:52 +0700

    ini php pertama saya

---
+++ string.php
@@ -0,0 +1,26 @@
+<?php
+
+//awalan
+$nama = "Fauzan Kurniadin";
+echo $nama;
+echo "</br>";
+
+//strlen()
+echo strlen($nama);
+echo "</br>";
+
+//str_word_count()
+echo str_word_count($nama);
+echo "</br>";
+
+//strrev()
+echo strrev($nama);
+echo "</br>";
+
+//strpos()
+echo strpos($nama, "Kurniadin");
+echo "</br>";
+
+//str_replace mengganti nama
+echo str_replace("Kurniadin", "Suiiiiiii", $nama);
+?>
